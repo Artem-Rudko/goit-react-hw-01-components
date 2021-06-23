7b1deb61310d072e7b9688aba6b51e9fe37bf359
Artem-Rudko <72283896+Artem-Rudko@users.noreply.github.com>
2021-06-24 01:45:26 +0300

    add slyles.module.css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 // import './App.css';
-import Profile from './components/profile/Profile';
-import Statistics from './components/statistics/Statistics';
-import user from './components/profile/user.json';
-import statisticalData from './components/statistics/statistical-data.json';
-import FriendList from './components/friend-list/FriendList';
-import friends from './components/friend-list/friends.json';
-import TransactionHistory from './components/transactionHistory/TransactionHistory';
-import transactions from './components/transactionHistory/TransactionHistory.json';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import user from './components/Profile/user.json';
+import statisticalData from './components/Statistics/statistical-data.json';
+import FriendList from './components/Friend-list/FriendList';
+import friends from './components/Friend-list/friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/TransactionHistory.json';
 
 function App() {
-  return (
-    <div className="App">
-      <Profile 
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats} />
+    return (
+        <div className="App">
+            <Profile
+                name={user.name}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}
+            />
 
-      <Statistics title="Upload stats" stats={statisticalData} />
+            <Statistics title="Upload stats" stats={statisticalData} />
 
-      <FriendList friends ={ friends } />
+            <FriendList friends={friends} />
 
-      <TransactionHistory transactions={ transactions } />
-    </div>
-  );
+            <TransactionHistory transactions={transactions} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/friend-list/FriendListItem.js
+++ src/components/friend-list/FriendListItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import defaultAvatar from '../../default-avatar.svg';
+import styles from './FriendList.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => (
-    <li className="item">
-        <span className="status">{isOnline}</span>
-        <img className="avatar" src={avatar} alt={name} width="48" />
-        <p className="name">{name}</p>
+    <li className={styles.item}>
+        <span
+            className={
+                isOnline ? styles.status__isOnline : styles.status__isOffline
+            }
+        >
+            {isOnline}
+        </span>
+        <img className={styles.avatar} src={avatar} alt={name} width="48" />
+        <p className={styles.name}>{name}</p>
     </li>
 );
 
